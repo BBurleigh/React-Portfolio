a21c8b6872e164f502dba52e4372777539f907ed
Brayden <brayburl224@gmail.com>
2023-01-14 14:48:11 -0500

    wrote a tiny arrow function in index.js in components/Layout folder to see if this would display when we run the app

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,1 +1,7 @@
 import './index.scss';
+
+const Layout = () => {
+    return <>Let's see if we have been precisely following the tutorial video. IF so, this will be tiny and show!</>
+}
+
+export default Layout;
