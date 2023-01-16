e9e606e6b1de5b120278007f5dd8c74507ecf574
Brayden <brayburl224@gmail.com>
2023-01-15 21:35:14 -0500

    worked on individual letter animation/setting this up in the home component, index.js

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -8,17 +8,24 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['Z', 'e', 'l', 'p', 'h'];
-    const jobArray = ['F', 'u', 'l', 'l', 's', 't', 'a', 'c', 'k', ' ', 'W', 'e', 'b', ' ', 'D', 'e', 'v', 'e','l', 'o', 'p', 'e', 'r', ' ', '/', ' ', 'E', 'd', 'u', 'c', 'a', 't', 'o', 'r', ' ', '/', ' ', 'W', 'r', 'i', 't', 'e', 'r']
+    const jobArray = ['a', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'i', 'n', 'g', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
 
     return (
         <div className = "container home-page">
             <div className = "text-zone">
-                <h1>Hello, <br /> I'm 
+                <h1>
+                    <span className = "letterClass">H</span>
+                    <span className = {`${letterClass} _10`}>e</span>
+                    <span className = {`${letterClass} _11`}>l</span>
+                    <span className = {`${letterClass} _12`}>l</span>
+                    <span className = {`${letterClass} _13`}>o,</span>
+                    <br />
+                    <span className = {`${letterClass} _14`}>I</span>
+                    <span className = {`${letterClass} _15`}>'m</span>
                 <img src = { LogoTitle } alt = "developer" />
-                <AnimatedLetters letterClass = {letterClass} strArray />
-                Zelph, 
+                <AnimatedLetters letterClass = {letterClass} strArray = {nameArray} idx = {18} />
                 <br />
-                a developing developer.
+                <AnimatedLetters letterClass = {letterClass} strArray = {jobArray} idx = {22} />
                 </h1>
                 <h2>FullStack Web Developer / Educator / Writer </h2>
                 <Link to = "/contact" className = "flat-button">Contact me, whether it's to say hello or about a potential project.</Link>
