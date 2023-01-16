51dc51c6f67d9fa83e8a20759925683b34f49397
Brayden <brayburl224@gmail.com>
2023-01-15 21:19:29 -0500

    working on animation within the home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,21 @@
 import './index.scss';
+import { useState } from 'react';
 import LogoTitle from '../../assets/images/logo-s.png';
+import AnimatedLetters from '../AnimatedLetters';
 import { Link } from 'react-router-dom';
 
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['Z', 'e', 'l', 'p', 'h'];
+    const jobArray = ['F', 'u', 'l', 'l', 's', 't', 'a', 'c', 'k', ' ', 'W', 'e', 'b', ' ', 'D', 'e', 'v', 'e','l', 'o', 'p', 'e', 'r', ' ', '/', ' ', 'E', 'd', 'u', 'c', 'a', 't', 'o', 'r', ' ', '/', ' ', 'W', 'r', 'i', 't', 'e', 'r']
 
     return (
         <div className = "container home-page">
             <div className = "text-zone">
                 <h1>Hello, <br /> I'm 
                 <img src = { LogoTitle } alt = "developer" />
+                <AnimatedLetters letterClass = {letterClass} strArray />
                 Zelph, 
                 <br />
                 a developing developer.
