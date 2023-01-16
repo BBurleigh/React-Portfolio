4cffcbc6b95d82ae81f8128fbfa6bc6b7b64e5d3
Brayden <brayburl224@gmail.com>
2023-01-15 22:08:23 -0500

    fixed issue with return for useEffect and need to tinker with last few letters following the appearance sequence

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,22 @@
 import './index.scss';
 import { useEffect, useState } from 'react';
-import LogoTitle from '../../assets/images/logo-s.png';
-import AnimatedLetters from '../AnimatedLetters';
 import { Link } from 'react-router-dom';
+// import Loader from 'react-loaders';
+import LogoTitle from '../../assets/images/logo-s.png';
+// import Logo from './Logo';
+import AnimatedLetters from '../AnimatedLetters';
+
 
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
     const nameArray = ['Z', 'e', 'l', 'p', 'h'];
-    const jobArray = ['a', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'i', 'n', 'g', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
+    const jobArray = ['a', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'i', 'n', 'g'];
+    const jobArray2 = ['d', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r'];
 
     useEffect(() => {
-        return setTimeout(() => {
-            setLetterClass('text-animate-hover')
+         setTimeout(() => {
+            return setLetterClass('text-animate-hover')
         }, 4000)
     }, [])
 
@@ -32,6 +36,8 @@ const Home = () => {
                 <AnimatedLetters letterClass = {letterClass} strArray = {nameArray} idx = {16} />
                 <br />
                 <AnimatedLetters letterClass = {letterClass} strArray = {jobArray} idx = {22} />
+                <br />
+                <AnimatedLetters letterClass = {letterClass} strArray = {jobArray2} idx = {34} />
                 </h1>
                 <h2>FullStack Web Developer / Educator / Writer </h2>
                 <Link to = "/contact" className = "flat-button">Contact me, whether it's to say hello or about a potential project.</Link>
