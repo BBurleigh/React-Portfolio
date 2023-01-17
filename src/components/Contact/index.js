7745e3068f94453dc622f717cb35e9302f51d6ec
Brayden <brayburl224@gmail.com>
2023-01-16 23:23:58 -0500

    working on getting the email to function, where a user can send a message directly to my email

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,14 @@
 import './index.scss';
 import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate');
+
+    const form = useRef();
 
     const contactTitle = ['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e'];
 
@@ -14,6 +17,22 @@ const Contact = () => {
            return setLetterClass('text-animate-hover')
        }, 3000)
    }, [])
+
+   const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+    .sendForm('gmail', 'template_lis3x97', e.target, 'h71gAcwrk3I-zQSU2')
+    .then(
+        () => {
+            alert("Your message has been sent, and I'll follow up with you in at most 24 hours!");
+            window.location.reload(false)
+            },
+        () => {
+            alert("There was an issue sending this. Please try again.")
+              }
+        )
+   }
     
     return (
         <div>
@@ -29,10 +48,10 @@ const Contact = () => {
                         Here is what I will put above my contact form...
                     </p>
                     <div className = "contact-form">
-                        <form>
+                        <form ref = {form} onSubmit = {sendEmail}>
                             <ul>
                                 <li className = "half">
-                                    <input type = "text" name = "name" placeholder = "Your full and preferred name" required />
+                                    <input type = "text" name = "name" placeholder = "Your preferred name" required />
                                 </li>
                                 <li className = "half">
                                     <input type = "email" name = "email" placeholder = "Your email address" required />
