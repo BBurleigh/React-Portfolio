356cbb554e338f97ef5b436540306f36bf8b4354
Brayden <brayburl224@gmail.com>
2023-01-16 22:17:34 -0500

    copied over animation code to the contact index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,12 +1,30 @@
 import './index.scss';
 import Loader from 'react-loaders';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
 
 const Contact = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    const contactTitle = ['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e'];
+
+    useEffect(() => {
+        setTimeout(() => {
+           return setLetterClass('text-animate-hover')
+       }, 3000)
+   }, [])
     
     return (
         <div>
             <div className = "container contact-page">
-            "Bob"
+                <div className = "text-zone">
+                    <h1>
+                        <AnimatedLetters
+                        strArray = {contactTitle} 
+                        idx = {15} />
+                    </h1>
+                </div>
             </div>
             <Loader type = "pacman" />
         </div>
