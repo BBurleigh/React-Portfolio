5244c36c3a117fc3f6c9d739524f1e694467661a
Brayden <brayburl224@gmail.com>
2023-01-16 21:10:35 -0500

    copied over useState and useEffect from Home index to About index in order to have same animation/hover effect

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,8 +1,17 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
     
+    useEffect(() => {
+        setTimeout(() => {
+           return setLetterClass('text-animate-hover')
+       }, 5000)
+   }, [])
+
     return (
         <div className = "container about-page">
             <div className = "text-zone">
