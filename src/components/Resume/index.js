1c47282fa94486e549639f0593cf575bc6797dbf
Brayden <brayburl224@gmail.com>
2023-01-18 11:09:34 -0500

    resume is properly displayed and downloadable; need to make sure my animation for the title works for this page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -9,13 +9,27 @@ const Resume = () => {
 
     const resumeTitle = "My Resume".split("");
 
-    const renderResume = () => {
-        return (<div className = "container resume-page">
-            "BLOB BLOB ResumeUME UME USER
-        </div>
-            
-        )
-    }
+    useEffect(() => {
+        setTimeout(() => {
+           return setLetterClass('text-animate-hover')
+       }, 3000)
+   }, [])
+
+        return (
+            <div>
+                <div className = "container resume-page">
+                    <h1 className = "resume-title">
+                        <AnimatedLetters
+                        letterClass = {letterClass}
+                        strArray = {resumeTitle}
+                        idx = {15}
+                        />
+                    </h1>
+                    <iframe className = "resume-container" src="https://drive.google.com/file/d/1leCnSuOcqdlvZ2yvqocCEFwQCludWt9y/preview" width="800" height="600" allow="autoplay"></iframe>
+                </div>
+            <Loader type = "pacman" />
+        </div>    
+    )
 }
 
 export default Resume;
