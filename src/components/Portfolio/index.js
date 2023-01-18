04106dfdff36cfa9469b8968c6ea32b1705e7206
Brayden <brayburl224@gmail.com>
2023-01-17 20:42:43 -0500

    displaying title and description on page as well as a button to click to take person to portfolio URL; need to fix image/placement so that it is not blocking

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -19,7 +19,13 @@ const Portfolio = () => {
                     return (
                         <div className = "image-box" key = {idx}>
                             <img className = "portfolio-image" src = {port.cover_image} alt = "portfolio image" />
+                            <div className = "content">
+                                <p className = "title">{port.title}</p>
+                                <h4 className = "description">{port.description}</h4>
+                                <button className = "btn" onClick = {() => window.open(port.url)}>View</button>
+                            </div>
                         </div>
+                        
                     )
                 })
             }
