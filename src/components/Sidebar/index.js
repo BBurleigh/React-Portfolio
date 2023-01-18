409d02ef1a19825599217a7940878eae1b24115c
Brayden <brayburl224@gmail.com>
2023-01-18 12:31:50 -0500

    changed sidebar logo and am adding personalized CSS to components

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,43 +1,42 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
+import LogoS from '../../assets/images/zelph_logo.PNG';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome,  faPaperclip, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
-import { faDiscourse, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className = "navbar">
         <Link className = "logo" to = "/">
             <img src ={LogoS} alt = "logo" />
-            <img className = "sub-logo" src ={LogoSubtitle} alt = "logo-subtitle" />
         </Link>
         <nav>
             <NavLink exact = "true" activeclassname = "active" to = "/">
-                <FontAwesomeIcon icon = {faHome} color = "#4d4d4e" />
+                <FontAwesomeIcon icon = {faHome} color = "#2e3138" />
             </NavLink>
             <NavLink exact = "true" activeclassname = "active" className = "about-link" to = "/about">
-                <FontAwesomeIcon icon = {faUser} color = "#4d4d4e" />
+                <FontAwesomeIcon icon = {faUser} color = "#2e3138" />
             </NavLink>
             <NavLink exact = "true" activeclassname = "active" className = "portfolio-link" to = "/portfolio">
-                <FontAwesomeIcon icon = {faSuitcase} color = "#4d4d4e" />
+                <FontAwesomeIcon icon = {faSuitcase} color = "#2e3138" />
             </NavLink>
             <NavLink exact = "true" activeclassname = "active" className = "resume-link" to = "/resume">
-                <FontAwesomeIcon icon = {faPaperclip} color = "#4d4d4e" />
+                <FontAwesomeIcon icon = {faPaperclip} color = "#2e3138" />
             </NavLink>
             <NavLink exact = "true" activeclassname = "active" className = "contact-link" to = "/contact">
-                <FontAwesomeIcon icon = {faEnvelope} color = "#4d4d4e" />
+                <FontAwesomeIcon icon = {faEnvelope} color = "#2e3138" />
             </NavLink>
         </nav>
         <ul>
             <li>
                 <a target = "_blank" rel = "noreferrer" href = "https://www.linkedin.com/in/brayden-burleigh-a893b91b3">
-                    <FontAwesomeIcon icon = {faLinkedin} color = "#4d4d4e" />
+                    <FontAwesomeIcon icon = {faLinkedin} color = "#2e3138" />
                 </a>
             </li>
             <li>
                 <a target = "_blank" rel = "noreferrer" href = "https://github.com/BBurleigh">
-                    <FontAwesomeIcon icon = {faGithub} color = "#4d4d4e" />
+                    <FontAwesomeIcon icon = {faGithub} color = "#2e3138" />
                 </a>
             </li>
         </ul>
